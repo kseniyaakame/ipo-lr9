bde1a257c3f74619c1fda68a4d001c263f0d44b9
Kseniya <147338017+kseniyaakame@users.noreply.github.com>
2024-12-18 16:25:36 +0300

    Task 2 complete

--- collision/CorrectRect.py
+++ collision/CorrectRect.py
@@ -8,4 +8,4 @@ def isCorrectRect(rec):
         if i[0][0] >= i[1][0] or i[0][1] >= i[1][1]:
             raise RectCorrectError(f'Один из прямоугольников некорректный номер {count}')
     return True
-            
+             
